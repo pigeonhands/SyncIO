aa3f7e3d05d721c1ccd169a4ec7f62f03e49bb90
Sam Mackenzie <sam.mackenzie@live.com>
2016-12-15 23:45:02 +1300

    init

--- SyncIO/Client/SyncIOClient.cs
+++ SyncIO/Client/SyncIOClient.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SyncIO
+namespace SyncIO.Client
 {
-    public class Class1
+    class SyncIOClient
     {
     }
 }
