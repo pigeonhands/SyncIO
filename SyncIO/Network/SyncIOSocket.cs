30d763875ba0712932284ff99ac5be809555b782
Sam M <sam.mackenzie@live.com>
2016-12-26 17:18:27 +1300

    Changed KeepAlive intervals.

--- SyncIO/Network/SyncIOSocket.cs
+++ SyncIO/Network/SyncIOSocket.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SyncIO.Network {
 
@@ -20,14 +15,13 @@ namespace SyncIO.Network {
         private byte[] socketOptions = null;
 
         public SyncIOSocket() {
-            uint dummy = 0;
+            var keepaliveTime = 30000;  //30 sec
+            var keepaliveInterval = 30000; //30 sec
 
-            var keepaliveTime = 20;
-            var keepaliveInterval = 20;
-            socketOptions = new byte[Marshal.SizeOf(dummy) * 3];
+            socketOptions = new byte[sizeof(uint) * 3];
             BitConverter.GetBytes((uint)(keepaliveTime)).CopyTo(socketOptions, 0);
-            BitConverter.GetBytes((uint)keepaliveTime).CopyTo(socketOptions, Marshal.SizeOf(dummy));
-            BitConverter.GetBytes((uint)keepaliveInterval).CopyTo(socketOptions, Marshal.SizeOf(dummy) * 2);
+            BitConverter.GetBytes((uint)keepaliveTime).CopyTo(socketOptions, sizeof(uint));
+            BitConverter.GetBytes((uint)keepaliveInterval).CopyTo(socketOptions, sizeof(uint) * 2);
         }
 
         protected virtual void Close() {
